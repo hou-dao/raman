05d329e907e1a49810b25fc105371e2856f506b4
hou-dao <houdao829@gmail.com>
2016-01-17 21:04:44 +0800

    modified default.json & gen_input.py: handle modes with various parameters

--- input/gen_input.py
+++ input/gen_input.py
@@ -17,49 +17,64 @@ if __name__ == '__main__':
     temp = 298.0*kt2au
     
     # syst
-    mat1 = np.random.rand(4,4)
-    mat2 = np.random.rand(4,4)
+    hams = np.diag(np.array([100.0,300.0,500.0]))*(1.0+0.0J)*cm2au
 
-    hams = (mat1+mat1.T)*(1.0+0.0J)*temp
-
-    qmat = (mat2+mat2.T)*0.5
-    qmds = np.array([np.dot(qmat,qmat)])*(1.0+0.0J)
+   #qmd1 = np.loadtxt('./qmod_0')
+    qmd1 = np.zeros((3,3),dtype=float)
+    qmd1[0,1] = qmd1[1,0] = 1.0/sqrt(2.0)
+    qmd1[1,2] = qmd1[2,1] = 1.0
+    qmds = np.array([np.dot(qmd1,qmd1)])*(1.0+0.0J)
 
     syst.init (ini['syst'],hams,qmds)
    
     # bath
     ini['bath']['temp'] = temp
-   #ini['bath']['jsdr'] = [{'lams':1.0,'omgs':5.0,'gams':1.0}]
-    ini['bath']['jdru'] = [{'lamd':50.0*cm2au,'gamd':100.0*cm2au}]
-    ini['bath']['nmod'] = qmds.shape[0]
+    ini['bath']['mode'] = [
+            {
+                'jdru': [{'lamd':50.0*cm2au,'gamd':50.0*cm2au}],
+                'jsdr': []
+            }
+        ]
+    ini['bath']['nmod'] = len(ini['bath']['mode'])
     ini['bath']['pade'] = 2
     ini['bath']['npsd'] = 1
     bath.init (ini['bath'])
     
     # hidx
-    ini['hidx']['lmax'] = 15
+    ini['hidx']['lmax'] = 30
     ini['hidx']['nmax'] = 30000
     ini['hidx']['ferr'] = 2.0e-6
 
     #proprho
     jsonInit = {"deom":ini,
                 "nonlinear":{
-                    "w1max": 800*cm2au,  # cm-1 convert to au
-                    "w2max": 800*cm2au,  # cm-1 convert to au
+                    "w1max": 1200*cm2au,  # cm-1 convert to au
+                    "w2max": 1200*cm2au,  # cm-1 convert to au
                     "dt":  1.0*fs2au,    # fs   convert to au
                     "nt1": 512,
                     "nt2": 512,
                     "nk": 16,
-                    "staticErr": 2.0e-9,
+                    "staticErr": 2.0e-8,
                     "sch_hei": "hei",
                     "dip0": "inp_dip0.mat",
                     "dip1": "inp_dip1.mat",
                     "dip2": "inp_dip2.mat",
                 }
             }
-    dipo0 = qmat*(1.0+0.0J)
-    dipo1 = np.dot(qmat,qmat)*(1.0+0.0J)
-    dipo2 = qmat*(1.0+0.0J)
+   #jsonInit = {"deom":ini,
+   #            "linear":{
+   #                "wmax": 1200*cm2au,  # cm-1 convert to au
+   #                "dt":  1.0*fs2au,    # fs   convert to au
+   #                "nt": 512,
+   #                "nk": 16,
+   #                "staticErr": 2.0e-9,
+   #                "dip0": "inp_dip0.mat",
+   #                "dip1": "inp_dip1.mat",
+   #            }
+   #        }
+    dipo0 = (qmd1)*(1.0+0.0J)
+    dipo1 = (qmd1)*(1.0+0.0J)
+    dipo2 = np.dot(qmd1,qmd1)*(1.0+0.0J)
     arma.save(dipo0,"inp_dip0.mat")
     arma.save(dipo1,"inp_dip1.mat")
     arma.save(dipo2,"inp_dip2.mat")
